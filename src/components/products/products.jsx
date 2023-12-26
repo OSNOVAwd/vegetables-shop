a0ff84c2532ae799dab73e1f8536ec2b07b52d60
OSNOVAwd <abdurahmanovmurodzon95@gmail.com>
2023-12-26 21:38:36 +0500

    products commit

--- src/components/products/products.jsx
+++ src/components/products/products.jsx
@@ -1,12 +1,18 @@
-import React from 'react'
+import {products} from "../../constants/data"
+import Product from './product'
 
 const Products = () => {
   return (
-    <div className='w-full h-[100vh] p-20'>
-
+    <div className="w-full h-[100vh] p-20">
+    <h1 className="text-4xl font-bold my-4">Our Vegetables</h1>
+    <p>
+      All Products
+    </p>
+    <div className="grid grid-cols-4 gap-4 py-20">
+      {products.map(item => (
+        <Product product={item}/>
+      ))}
     </div>
-
-    
     </div>
   )
 }
